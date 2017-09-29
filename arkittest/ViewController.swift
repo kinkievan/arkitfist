335ac2b661a28e691875a1ee368e335d31e9444c
kinki <kinkievan@hotmail.com>
2017-09-29 08:56:42 +0800

    arkit

--- arkittest/ViewController.swift
+++ arkittest/ViewController.swift
@@ -11,33 +11,23 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+     let configuration = ARWorldTrackingConfiguration()
+ 
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
+        sceneView.session.run(configuration)
         let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
         sceneView.scene = scene
+       
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
+       
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -77,4 +67,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    @IBAction func resetButtonPressed(_ sender: Any) {
+        self.restartSession()
+  
+    }
+    func restartSession() {
+        sceneView.session.pause()
+        sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            node.removeFromParentNode()
+        }
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        sceneView.scene = scene
+        
+    }
+    
+  
 }
